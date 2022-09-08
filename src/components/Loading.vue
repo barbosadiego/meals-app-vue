b4a95e7ce67997fa14a7614e7b9d9340bd5d0a1b
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-08 09:53:44 -0300

    create loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="loading">
+    <div class="loading__icon"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+.loading{
+  width: 100%;
+  min-height: 100vh;
+  background-color: transparentize($color: #000000, $amount: .4);
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__icon{
+    display: block;
+    width: 80px;
+    height: 80px;
+    background-color: transparent;
+    border-radius: 50%;
+    border: 10px solid var(--white);
+    border-top-color: transparent;
+    animation: rotation 1s ease infinite ;
+  }
+
+  @keyframes rotation {
+    to{
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
